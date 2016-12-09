525a72a706512c94b2d04a448b1eb509fb6a4444
Kirk Byers <ktbyers@twb-tech.com>
2016-12-09 15:26:47 -0800

    Restoring author; rolling version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='getdrip',
-      version='0.2',
+      version='1.3',
       description='Python wrapper for https://www.getdrip.com/',
       url='https://github.com/nawarkhede/getdrip',
-      author='Kirk Byers',
-      author_email='ktbyersx@gmail.com',
+      author='Nishant Nawarkhede',
+      author_email='nishant.nawarkhede@gmail.com',
       license='MIT',
       packages=['getdrip'],
       zip_safe=False)
